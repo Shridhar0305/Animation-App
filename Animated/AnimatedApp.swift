51c216feed47434261452fe863fca0c0247f9bd5
vashi024 <vashi024@umn.edu>
2023-06-05 14:47:59 -0500

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Shridhar Vashishtha on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
